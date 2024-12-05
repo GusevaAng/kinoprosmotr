b5cbd1531ae0230bb6158550c767feea84503dba
Angelina Guseva <guseva.ang@gmail.com>
2024-12-05 16:38:48 +0500

    Fixing the problem with images in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import styles from './Navbar.module.scss'
 import { Link } from 'react-router-dom'
 import icon_home_white from '../../assets/navbar/icon-home-white.png'
 import icon_home_pink from '../../assets/navbar/icon-home-pink.png'
+import icon_movies_white from '../../assets/navbar/icon-movies-white.png'
+import icon_movies_pink from '../../assets/navbar/icon-movies-pink.png'
+import icon_series_white from '../../assets/navbar/icon-series-white.png'
+import icon_series_pink from '../../assets/navbar/icon-series-pink.png'
+import icon_cartoons_white from '../../assets/navbar/icon-cartoons-white.png'
+import icon_cartoons_pink from '../../assets/navbar/icon-cartoons-pink.png'
+import icon_awards_white from '../../assets/navbar/icon-awards-white.png'
+import icon_awards_pink from '../../assets/navbar/icon-awards-pink.png'
+import icon_actors_white from '../../assets/navbar/icon-actors-white.png'
+import icon_actors_pink from '../../assets/navbar/icon-actors-pink.png'
 
 
 const navbarItems = [
@@ -14,32 +24,32 @@ const navbarItems = [
     },
     {
         link: '/films',
-        image: '/src/assets/navbar/icon-movies-white.png',
-        imageHover: '/src/assets/navbar/icon-movies-pink.png',
+        image: icon_movies_white,
+        imageHover: icon_movies_pink,
         label: 'Фильмы'
     },
     {
         link: '/series',
-        image: '/src/assets/navbar/icon-series-white.png',
-        imageHover: '/src/assets/navbar/icon-series-pink.png',
+        image: icon_series_white,
+        imageHover: icon_series_pink,
         label: 'Сериалы'
     },
     {
         link: '/cartoons',
-        image: '/src/assets/navbar/icon-cartoons-white.png',
-        imageHover: '/src/assets/navbar/icon-cartoons-pink.png',
+        image: icon_cartoons_white,
+        imageHover: icon_cartoons_pink,
         label: 'Мультфильмы'
     },
     {
         link: '/awards',
-        image: '/src/assets/navbar/icon-awards-white.png',
-        imageHover: '/src/assets/navbar/icon-awards-pink.png',
+        image: icon_awards_white,
+        imageHover: icon_awards_pink,
         label: 'Награды'
     },
     {
         link: '/actors',
-        image: '/src/assets/navbar/icon-actors-white.png',
-        imageHover: '/src/assets/navbar/icon-actors-pink.png',
+        image: icon_actors_white,
+        imageHover: icon_actors_pink,
         label: 'Актеры'
     }
 ]
